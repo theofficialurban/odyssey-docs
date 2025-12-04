fee362b6e8b355e8e9a666561bf237a0721608f2
𝔘𝔯𝔟𝔞𝔫 <theofficialurban@protonmail.com>
2025-12-03 22:21:14 -0500

    try

--- docs/.vitepress/theme/components/Card.vue
+++ docs/.vitepress/theme/components/Card.vue
@@ -57,13 +57,13 @@ defineProps({
     <div class="p-6 md:col-span-3">
       <slot name="title">
         <h1
-          class="mb-2 text-fluid-base font-bold text-gray-100 group-hover:text-cyan-400"
+          class="mb-2 text-sm lg:text-[20px] font-bold text-gray-100 group-hover:text-cyan-400"
         >
           {{ title }}
         </h1>
       </slot>
 
-      <p v-if="description" class="text-fluid-sm text-gray-400">
+      <p v-if="description" class="text-sm lg:text-lg text-gray-400">
         {{ description }}
       </p>
       <sub class="font-mono">{{ href }}</sub>
