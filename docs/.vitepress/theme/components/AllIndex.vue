8f5340b81540aef0d4352267530cac3aa397129e
𝔘𝔯𝔟𝔞𝔫 <theofficialurban@protonmail.com>
2026-06-19 17:41:18 -0400

    try all

--- docs/.vitepress/theme/components/AllIndex.vue
+++ docs/.vitepress/theme/components/AllIndex.vue
@@ -1,14 +1,70 @@
 <script setup lang="ts">
 import { useData } from "vitepress";
 import { computed, ComputedRef } from "vue";
-import CollectionIndex from "./CollectionIndex.vue";
-const { theme } = useData();
 
-const collections: ComputedRef<string[]> = computed(() =>
-  Object.keys(theme.value.collections)
-);
+import {
+  VPLCollectionItems,
+  VPLCollectionPage,
+  VPLCollectionPageSection,
+  VPLCollectionPageTags,
+  useCollection,
+  VPLCollectionPageTitle,
+} from "@lando/vitepress-theme-default-plus";
+import {
+  Collection,
+  CollectionDefinition,
+  findCollection,
+} from "../../utils.js";
+
+const { theme } = useData();
+const { hasItems, tags } = useCollection();
+
+type ColObj = [
+  cKey: string,
+  cDef: CollectionDefinition | null,
+  c: Collection,
+  hasPosts: ComputedRef<boolean>,
+];
+
+const collectionsKeys = Object.keys(theme.value.collections);
+const collections: ComputedRef<ColObj[]> = computed(() => {
+  return collectionsKeys.map((co) => {
+    const foundCol: Collection = useCollection(co);
+
+    return [
+      co,
+      findCollection(theme.value.collections, co),
+      foundCol,
+      computed(() => hasItems(foundCol.pages, tags)),
+    ];
+  });
+});
 </script>
 
 <template>
-  <CollectionIndex title="All Posts" lead="" :collection="collections" />
+  <VPLCollectionPage>
+    <VPLCollectionPageTitle>
+      <template #title> All Posts </template>
+      <template #lead> A listing of all pages on the site! </template>
+    </VPLCollectionPageTitle>
+
+    <VPLCollectionPageTags v-model="tags" />
+
+    <VPLCollectionPageSection
+      v-for="[cKey, cDef, col, hasPosts] in collections"
+    >
+      <template #title>
+        <Icon
+          v-if="cDef !== null && cDef.icon && cDef.iconLink"
+          :icon="cDef.icon ?? ''"
+          :link="cDef.iconLink"
+          :title="cKey.toUpperCase()"
+        />
+      </template>
+
+      <template #items>
+        <VPLCollectionItems :items="col.pages" :tags="tags" />
+      </template>
+    </VPLCollectionPageSection>
+  </VPLCollectionPage>
 </template>
